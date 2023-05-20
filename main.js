1b86b2688d85a167b4e2b401269e4303eca68832
HARISH SS <116174706+harishss20@users.noreply.github.com>
2023-05-20 20:33:29 +0530

    I change the add and remove the todo list

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 let inputList = document.getElementById("input-list");
 let inputBtn = document.getElementById("input-btn");
 let tempStore = document.getElementById("responses");
+let todoContainer = document.getElementById("todo-container");
 inputBtn.addEventListener("click", () => {
-  tempStore.innerText = inputList.value;
+  if (inputList.value === "") {
+    alert("Please Type your daily routine List");
+  } else {
+    todoContainer.innerHTML += `<li class="todo-item"><div>${inputList.value}</div> <div class="fa" onclick="removeFoodItem(event)">X</div></li>`;
+  }
 });
+function removeFoodItem(event) {
+  let todoRemove = event.target.parentNode;
+  todoRemove.remove();
+}
