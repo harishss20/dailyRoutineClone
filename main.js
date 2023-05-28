5f1a7b080a06a6bb85d8e30974997532585cfca7
HARISH SS <116174706+harishss20@users.noreply.github.com>
2023-05-28 20:23:45 +0530

    change in UI

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 let inputList = document.getElementById("input-list");
 let inputBtn = document.getElementById("input-btn");
 let tempStore = document.getElementById("responses");
+const newList = document.createElement("li");
 let todoContainer = document.getElementById("todo-container");
 let inputContainer = document.getElementById("input-container");
+let inputContainerSec = document.getElementById("input-container2");
+let imgDiv = document.getElementById("imgEl");
+
+//create div element after the ul element
+const listNo = document.createElement("div");
+listNo.className = "showList";
+inputContainerSec.prepend(listNo);
+
+//call back function for add button
 let inputGetEl = () => {
   if (inputList.value === "") {
     alert("Please Type your daily routine List");
@@ -23,15 +33,19 @@ let inputGetEl = () => {
     todoContainer.append(newList);
     // to reset the input value
     inputList.value = "";
+    addList();
   }
 };
+
+inputBtn.addEventListener("click", inputGetEl); //Event for add button
+
 //remove the list
-inputBtn.addEventListener("click", inputGetEl);
 function removeTodoItem(event) {
   let todoRemove = event.target.parentNode;
   todoRemove.remove();
+  addList();
 }
-// enter key
+// enter key for the input value
 inputContainer.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     inputGetEl();
@@ -39,3 +53,15 @@ inputContainer.addEventListener("keyup", (event) => {
     inputContainer.value = "";
   }
 });
+
+//better UI
+function addList() {
+  if (todoContainer.children.length > 0) {
+    listNo.textContent = `You have ${todoContainer.children.length} work`;
+    imgDiv.hidden = true;
+    listNo.hidden = false;
+  } else {
+    imgDiv.hidden = false;
+    listNo.hidden = true;
+  }
+}
